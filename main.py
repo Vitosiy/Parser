d48b856ae9b8ef06ade608a267bf1eaf8ca82973
Виталий Вересников <Vitosiy21@yandex.ru>
2021-06-29 17:50:47 +0500

    Done

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import datetime as dt
 import pandas as pd
 
 # Opening the connection and grabbing the page
@@ -23,44 +22,47 @@ loc = element.location
 size = element.size
 
 
-print(loc)
-print(size)
+#print(loc)
+#print(size)
 
 
 action.move_to_element_with_offset(element, 1, 0).perform()
 
+infected = [driver.find_element_by_xpath('/html/body/main/section/div/section[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[2]/div[1]/div[3]/div/div[20]/div[1]/span[1]').text]
+death = [driver.find_element_by_xpath('/html/body/main/section/div/section[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[2]/div[1]/div[3]/div/div[20]/div[2]/span').text]
+date = [driver.find_element_by_xpath('/html/body/main/section/div/section[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[2]/div[1]/div[3]/div/div[20]/div[4]/span').text]
 
-infected = driver.find_element_by_xpath('/html/body/main/section/div/section[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[2]/div[1]/div[3]/div/div[20]/div[1]/span[1]').text
-death = driver.find_element_by_xpath('/html/body/main/section/div/section[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[2]/div[1]/div[3]/div/div[20]/div[2]/span').text
-date = driver.find_element_by_xpath('/html/body/main/section/div/section[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[2]/div[1]/div[3]/div/div[20]/div[4]/span').text
-
-# print(infected)
-# print(death)
-# print(date)
-
-
-dictionary = {}
-dictionary[date] = infected + death
 
 pace = -1
 
+
 while True:
     action.move_by_offset(pace, 0).perform()
-    infected = driver.find_element_by_xpath(
+    infected_ = driver.find_element_by_xpath(
         '/html/body/main/section/div/section[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[2]/div[1]/div[3]/div/div[20]/div[1]/span[1]').text
-    death = driver.find_element_by_xpath(
+    death_ = driver.find_element_by_xpath(
         '/html/body/main/section/div/section[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[2]/div[1]/div[3]/div/div[20]/div[2]/span').text
-    date = driver.find_element_by_xpath(
+    date_ = driver.find_element_by_xpath(
         '/html/body/main/section/div/section[1]/div/div/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[2]/div[1]/div[3]/div/div[20]/div[4]/span').text
 
-    if infected == "32" and death == "0" and date == "12 МАР":
+
+    if infected_ == "32" and death_ == "0" and date_ == "12 МАР":
+        infected.append(infected_)
+        death.append(death_)
+        date.append(date_)
         break
 
-    if date in dictionary:
+    if (date_ in date) and (death_ in death) and (infected_ in infected):
         pass
     else:
-        dictionary[date] = infected + death
+        infected.append(infected_)
+        death.append(death_)
+        date.append(date_)
+
+dictionary = {"date": date, "infected": infected, "death": death}
 
 driver.quit()
 
-df = pd.DataFrame.from_dict(dictionary, orient='index')
+df = pd.DataFrame.fromdict(dictionary)
+
+df.to_csv("corona_yan.csv")
